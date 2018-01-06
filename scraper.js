f7d5a9bfa4b0d86f3a808ff39de737bbeea414cd
MuteBard <csevere@brandeis.edu>
2018-01-06 04:14:06 -0500

    scraper takes in 3 links, removed comma's from numbers more than 3 digits

--- scraper.js
+++ scraper.js
@@ -6,25 +6,32 @@ const cheerio = require('cheerio');
 let type = ["Headgear","Clothing","Shoes"]
 let str = ''
 
-url = //"https://splatoonwiki.org/wiki/List_of_headgear_in_Splatoon_2";
-       // "https://splatoonwiki.org/wiki/List_of_clothing_in_Splatoon_2";
-       "https://splatoonwiki.org/wiki/List_of_shoes_in_Splatoon_2";
-axios(url).then(html => {
-    const $ = cheerio.load(html.data)
-    var allgear = $('tr').text((idx, data) => {
-        if (idx > 0){
-            let gearPieceString = data.split()[0]
-            let gearPieceArray = gearPieceString.split("\n")
-            //crafting sql insertion query
-            str += 
-            `(DEFAULT,"${gearPieceArray[2].trim()}", "",
-            "${gearPieceArray[3].trim()}", "${gearPieceArray[5].trim()}", "...", "...",
-            "F","${type[2]}",
-            "-","-","-","-",
-            ${(!isNaN(gearPieceArray[4]) || gearPieceArray[4].includes(',') ? gearPieceArray[4].trim() : "NULL" )}, 0),\n`
-        }
-    })
-}).then(() => {
-    console.log(str.substring(0 , str.length-2))
-}).catch(err => console.log(err))
+urls = ["https://splatoonwiki.org/wiki/List_of_headgear_in_Splatoon_2",
+       "https://splatoonwiki.org/wiki/List_of_clothing_in_Splatoon_2",
+       "https://splatoonwiki.org/wiki/List_of_shoes_in_Splatoon_2"]
        
+urls.forEach((url, udx) =>{
+    axios(url).then(html => {
+        const $ = cheerio.load(html.data)
+        var allgear = $('tr').text((idx, data) => {
+            if (idx > 0){
+                let gearPieceString = data.split()[0]
+                let gearPieceArray = gearPieceString.split("\n")
+
+                //remove all commas from numbers
+                gearPieceArray[4] = gearPieceArray[4].replace(',',"")
+
+                //crafting sql insertion query
+                str += 
+                `(DEFAULT,"${gearPieceArray[2].trim()}", "",
+                "${gearPieceArray[3].trim()}", "${gearPieceArray[5].trim()}", "...", "...",
+                "F","${type[udx]}",
+                "-","-","-","-",
+                ${(!isNaN(gearPieceArray[4]) ? gearPieceArray[4].trim() : "NULL" )}, 0),\n`
+            }
+        })
+    }).then(() => {
+        console.log(str)
+    }).catch(err => console.log(err))
+})   
+
